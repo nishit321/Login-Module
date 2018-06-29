fbb4db4e5d91b5f1bb6ed2d2d94c177d892ff04a
nishit.shah44 <nishit.shah44@gmail.com>
2018-06-29 15:54:50 +0530

    Route Secure using isAuthenticated function

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 // Get HomePage
-router.get('/',(req,res)=>{
+router.get('/',ensureAuthentication,(req,res)=>{
     res.render('index');
 });
+
+function ensureAuthentication(req,res,next) {
+    if(req.isAuthenticated()){
+        return next();
+    }else{
+        req.flash('error_msg','You are not logged in');
+        res.redirect('users/login');
+    }
+}
 
 module.exports = router;
